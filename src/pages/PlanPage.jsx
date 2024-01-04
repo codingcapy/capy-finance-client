883dc27eccbdc15674ae75b80bfe1d0d28c6812e
Paul <spkim0921@gmail.com>
2024-01-03 18:36:42 -0800

    5th commit
    implementing income submission

--- src/pages/PlanPage.jsx
+++ src/pages/PlanPage.jsx
@@ -75,6 +75,31 @@ export default function PlanPage() {
     const [submitAssetMode, setSubmitAssetMode] = useState(false)
     const [expandedLiabilities, setExpandedLiabilities] = useState(false)
     const [submitLiabilityMode, setSubmitLiabilityMode] = useState(false)
+    const [incomeMessage, setIncomeMessage] = useState("")
+
+    async function submitIncome(e) {
+        e.preventDefault()
+        const title = e.target.title.value
+        const content = e.target.content.value
+        const value = e.target.value.value
+        if (isNaN(value)) {
+            setIncomeMessage("Value must be a numeric value");
+            return;
+        }
+        const taxRate = e.target.taxrate.value
+        if (isNaN(taxRate)) {
+            setIncomeMessage("Value must be a numeric value");
+            return;
+        }
+        const startDate = e.target.startdate.value
+        const endDate = e.target.enddate.value
+        const planId = data.plan.planId
+        const newIncome = { title, content, value, taxRate, startDate, endDate, planId }
+        const res = await axios.post(`${DOMAIN}/api/v1/incomes`, newIncome)
+        if (res?.data.success) {
+            navigate(`/plans/${data.plan.planId}`)
+        }
+    }
 
     return (
         <div>
@@ -90,18 +115,30 @@ export default function PlanPage() {
                     <div className="flex text-xl font-bold text-center pt-5" onClick={() => setExpandedIncome(!expandedIncome)}>Income {expandedIncome ? <FaChevronUp size={20} className=" text-center ml-5" /> : <FaChevronDown size={20} className=" text-center ml-5" />}</div>
                     {expandedIncome && <button className="rounded-xl my-5 py-2 px-2 bg-slate-600 text-white" onClick={() => setSubmitIncomeMode(true)}>Add Income</button>}
                     {submitIncomeMode ?
-                        <form className="flex flex-col">
+                        <form onSubmit={submitIncome} className="flex flex-col">
                             <div className="flex flex-col">
                                 <label htmlFor="title" >Title</label>
                                 <input type="text" name='title' id='title' placeholder="Title" required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
                             </div>
-                            <div className="flex flex-col my-1">
+                            <div className="flex flex-col mt-1">
                                 <label htmlFor="content">Description</label>
                                 <textarea type="text" name='content' id='content' placeholder='Content' required rows="2" cols="40" className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
                             </div>
-                            <div className="flex flex-col my-1">
+                            <div className="flex flex-col mt-1">
                                 <label htmlFor="value">Value</label>
                                 <input type="text" name='value' id='value' placeholder='Value' required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="taxrate">Tax Rate</label>
+                                <input type="text" name='taxrate' id='taxrate' placeholder='Tax Rate' required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="startdate">Start Date</label>
+                                <input type="date" name='startdate' id='startdate' required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="enddate">End Date</label>
+                                <input type="date" name='enddate' id='enddate' className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
                             </div>
                             <button type="submit" className="rounded-xl my-1 py-2 px-2 bg-slate-700 text-white">Add</button>
                             <button className="rounded-xl py-2 px-2 bg-red-900 text-white" onClick={() => setSubmitIncomeMode(false)}>Cancel</button>
@@ -126,6 +163,7 @@ export default function PlanPage() {
                                 <label htmlFor="value">Value</label>
                                 <input type="text" name='value' id='value' placeholder='Value' required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
                             </div>
+                            {incomeMessage}
                             <button type="submit" className="rounded-xl my-1 py-2 px-2 bg-slate-700 text-white">Add</button>
                             <button className="rounded-xl py-2 px-2 bg-red-900 text-white" onClick={() => setSubmitFixedMode(false)}>Cancel</button>
                         </form>
@@ -155,11 +193,47 @@ export default function PlanPage() {
                 </div>
                 <div>
                     <div className="flex text-xl font-bold text-center py-5 " onClick={() => setExpandedAssets(!expandedAssets)}>Assets {expandedAssets ? <FaChevronUp size={20} className=" text-center ml-5" /> : <FaChevronDown size={20} className=" text-center ml-5" />}</div>
-                    {expandedAssets && <button className="rounded-xl my-5 py-2 px-2 bg-slate-600 text-white">Add Asset</button>}
+                    {expandedAssets && <button className="rounded-xl my-5 py-2 px-2 bg-slate-600 text-white" onClick={() => setSubmitAssetMode(true)}>Add Asset</button>}
+                    {submitAssetMode ?
+                        <form className="flex flex-col">
+                            <div className="flex flex-col">
+                                <label htmlFor="title" >Title</label>
+                                <input type="text" name='title' id='title' placeholder="Title" required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="content">Description</label>
+                                <textarea type="text" name='content' id='content' placeholder='Content' required rows="2" cols="40" className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="value">Value</label>
+                                <input type="text" name='value' id='value' placeholder='Value' required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <button type="submit" className="rounded-xl my-1 py-2 px-2 bg-slate-700 text-white">Add</button>
+                            <button className="rounded-xl py-2 px-2 bg-red-900 text-white" onClick={() => setSubmitAssetMode(false)}>Cancel</button>
+                        </form>
+                        : ""}
                 </div>
                 <div>
                     <div className="flex text-xl font-bold text-center py-5 " onClick={() => setExpandedLiabilities(!expandedLiabilities)}>Liabilities {expandedLiabilities ? <FaChevronUp size={20} className=" text-center ml-5" /> : <FaChevronDown size={20} className=" text-center ml-5" />}</div>
-                    {expandedLiabilities && <button className="rounded-xl my-5 py-2 px-2 bg-slate-600 text-white">Add Liability</button>}
+                    {expandedLiabilities && <button className="rounded-xl my-5 py-2 px-2 bg-slate-600 text-white" onClick={() => setSubmitLiabilityMode(true)}>Add Liability</button>}
+                    {submitLiabilityMode ?
+                        <form className="flex flex-col">
+                            <div className="flex flex-col">
+                                <label htmlFor="title" >Title</label>
+                                <input type="text" name='title' id='title' placeholder="Title" required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="content">Description</label>
+                                <textarea type="text" name='content' id='content' placeholder='Content' required rows="2" cols="40" className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <div className="flex flex-col my-1">
+                                <label htmlFor="value">Value</label>
+                                <input type="text" name='value' id='value' placeholder='Value' required className="px-2 border rounded-lg border-slate-700 py-1 text-black" />
+                            </div>
+                            <button type="submit" className="rounded-xl my-1 py-2 px-2 bg-slate-700 text-white">Add</button>
+                            <button className="rounded-xl py-2 px-2 bg-red-900 text-white" onClick={() => setSubmitLiabilityMode(false)}>Cancel</button>
+                        </form>
+                        : ""}
                 </div>
             </div>
 
